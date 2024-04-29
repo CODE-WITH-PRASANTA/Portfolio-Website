5bc8712442f4596db87cf8ed0406748ae0e3f5b6
CODE-WITH-PRASANTA <prasantakhuntia2018@gmail.com>
2024-04-29 19:16:57 +0530

    adding my photos

---
+++ src/Components/Projects/Project.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Project.css'
+
+const Project = () => {
+    return (
+        <section className='Projects'>
+            Hello World
+        </section>
+    )
+}
+
+export default Project
